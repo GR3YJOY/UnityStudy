86af0618ab563a3ae8564ba8517470d3a4f56e67
JOKWIAE <gkdiqkf@naver.com>
2022-07-12 17:22:35 +0900

    2022-07-12 수업예제

---
+++ FlaffyBird/Assets/PipeObjectMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeObjectMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x - 0.03f, transform.position.y, 0);
+        if (transform.position.x <= -7.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    /*
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - 0.03f, transform.position.y, 0);
+        if (transform.position.x <= -7.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    */
+}
