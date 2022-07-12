d1fff5d1a3ed63cb091f664121ff78125bddfd44
JOKWIAE <gkdiqkf@naver.com>
2022-07-12 14:48:16 +0900

    2022-07-12 수업예제

---
+++ 1302_VR_0712/Assets/goTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goTest : MonoBehaviour
+{
+    public GameObject goModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goModel.transform.position = Vector3.zero; //zero : (0,0,0)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
